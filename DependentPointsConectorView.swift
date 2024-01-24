bdbd51e5b5418c768b01ffc20e1997113f107fca
Mihail <153065029+zuravlevmihail@users.noreply.github.com>
2024-01-24 17:46:25 +0100

    Update DependentPointsConectorView.swift

--- DependentPointsConectorView.swift
+++ DependentPointsConectorView.swift
@@ -40,3 +40,24 @@ struct DependentPointsConectorView: View {
             createButtonBody(with: "Cancel")
         }
     }
+                           
+    private var pointsIds: Set<String> {
+        store.state.selectedPointsIDs
+    }
+    
+    private func createButtonBody(with text: String) -> some View {
+        Capsule()
+            .fill(Color(uiColor: .systemGray3))
+            .frame(width: 100, height: 40)
+            .overlay {
+                Text(text)
+            }
+    }
+}
+
+struct DependentPointsConectorView_Previews: PreviewProvider {
+    static var previews: some View {
+        DependentPointsConectorView()
+            .environmentObject(AppStore.preview)
+    }
+}
